86e6b6489e41dd5eb1c87265d14e150dc6f0e56e
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2024-11-13 22:30:09 -0300

    a

--- src/models/organization.model.go
+++ src/models/organization.model.go
@@ -6,7 +6,6 @@ type Organization struct {
 	OrganizationID   string         `json:"organizationID" bson:"organizationID" binding:"required,min=1"`
 	OrganizationName string         `json:"organizationName" bson:"organizationName" binding:"required,min=1"`
 	BillingPlan      string         `json:"billingPlan" bson:"billingPlan" binding:"required"`
-	AiConfig         AiConfig       `json:"aiConfig" bson:"aiConfig" binding:"required"`
 	FrontendConfig   FrontendConfig `json:"frontendConfig" bson:"frontendConfig" binding:"required"`
 	CreatedTs        time.Time      `json:"createdTs" bson:"createdTs" binding:"required"`
 	Deleted          Deleted        `json:"deleted" bson:"deleted" binding:"required"`
@@ -19,11 +18,8 @@ type Deleted struct {
 }
 
 type FrontendConfig struct {
-	MinMsgAmmount uint `json:"minMsgAmmount" bson:"minMsgAmmount" binding:"required"`
-	MsgCharLimit  uint `json:"msgCharLimit" bson:"msgCharLimit" binding:"required"`
-}
-
-type AiConfig struct {
-	Behavior string `json:"behavior" bson:"behavior" binding:"required"`
-	Greet    string `json:"greet" bson:"greet" binding:"required"`
+	PrimaryColor   string
+	SecondaryColor string
+	MinMsgAmmount  uint `json:"minMsgAmmount" bson:"minMsgAmmount" binding:"required"`
+	MsgCharLimit   uint `json:"msgCharLimit" bson:"msgCharLimit" binding:"required"`
 }
