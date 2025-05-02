86b8a49d045b35c272e6ae7f7ece6ccce5e50283
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-05-02 13:33:30 -0300

    fix: common error names

--- src/common/errors.common.go
+++ src/common/errors.common.go
@@ -3,7 +3,7 @@ package common
 import "errors"
 
 var (
-	ErrAuth                = errors.New("authError")
-	ErrDbConflict          = errors.New("dbConflictError")
+	ErrAuth                = errors.New("auth error")
+	ErrDbConflict          = errors.New("db conflict error")
 	ErrDbTransactionCreate = errors.New("could not create DB transaction")
 )
