97f8f7f5676b54b313a6c4f8a25d3fdd06f6dd50
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-05-09 13:23:40 -0300

    docs: for it pkg

--- src/pkg/it/tools.go
+++ src/pkg/it/tools.go
@@ -1,6 +1,8 @@
 package it
 
 // Must ensures the the expression does not error, it panics in case of an error.
+// It is tipically used on the main func, where the error would be fatal, such as
+// connecting to a db server etc.
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
