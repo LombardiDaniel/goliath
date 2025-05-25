93ca3d32f80d07e24f37133808cfac625931653f
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2025-05-25 20:04:15 -0300

    docs

--- src/pkg/it/tools.go
+++ src/pkg/it/tools.go
@@ -2,10 +2,19 @@ package it
 
 // Must ensures the the expression does not error, it panics in case of an error.
 // It is tipically used on the main func, where the error would be fatal, such as
-// connecting to a db server etc.
+// connecting to a db server etc. Returns the first arg
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
 	}
 	return t
+}
+
+// MustNotErr ensures the the expression does not error, it panics in case of an error.
+// It is tipically used on the main func, where the error would be fatal, such as
+// connecting to a db server etc.
+func MustNotErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- src/pkg/token/hash.common.go
+++ src/pkg/token/hash.common.go
@@ -4,6 +4,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// HashPassword generates a bcrypt hash of the given password string.
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -12,6 +13,8 @@ func HashPassword(password string) (string, error) {
 	return string(bytes), nil
 }
 
+// CheckPasswordHash compares a plaintext password with a bcrypt hashed password.
+// Returns true if the password matches the hash, false otherwise.
 func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
